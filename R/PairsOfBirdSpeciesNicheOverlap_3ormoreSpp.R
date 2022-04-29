4b77d99cbd1382fbfd157546335373dcb8bf1de9
kaiyaprovost <17089935+kaiyaprovost@users.noreply.github.com>
2022-04-29 14:15:24 -0400

    ENH: add points to polygons for single polygon

--- R/PairsOfBirdSpeciesNicheOverlap_3ormoreSpp.R
+++ R/PairsOfBirdSpeciesNicheOverlap_3ormoreSpp.R
@@ -1043,6 +1043,60 @@ locatePolygonPoints = function(test_points,
 #' checked = subspeciesMatchChecker(locfile = polyLocations)
 #' checked_suspect = checked$suspect
 #' checked_good = checked$good
+
+
+#' Point to Polygon Locator 2
+#'
+#' This function takes a one polygon and a list of points
+#' and determines whether the points fall within the polygon, then
+#' adds a column for each polygon's name with a boolean of whether
+#' the point overlaps.
+#'
+#' @param test_points The points to check where they fall
+#' @param polygonA The  polygon
+#' @param setcoord Whether to set a coordinate reference system to the points and polygons
+#' @param crs A coordinate reference system to assign to the polygons and the test points if setcoord = TRUE
+#' @param nameA The (subspecies) name associated with the first polygon
+#'
+#' @export
+#' @examples
+#'
+#' listFromSubspeciesOcc = subspeciesOccQuery(spp="Cardinalis sinuatus",
+#'    subsppList=c("sinuatus","peninsulae","fulvescens"),pointLimit=100,dbToQuery="gbif")
+#' labeledLoc = labelSubspecies(subsppOccList=listFromSubspeciesOcc)
+#' locs = labeledLoc[labeledLoc$subspecies=="sinuatus",]
+#' locs_sin = labeledLoc[labeledLoc$subspecies=="sinuatus",]
+#' locs_ful = labeledLoc[labeledLoc$subspecies=="fulvescens",]
+#' dens_sin = subspeciesDensityMap(localities=locs_sin,quantile=0.95,
+#'    xmin=-125,xmax=-60,ymin=10,ymax=50)
+#' dens_ful = subspeciesDensityMap(localities=locs_ful,quantile=0.95,
+#'    xmin=-125,xmax=-60,ymin=10,ymax=50)
+#' densPol_sin = densityMapToPolygons(densityMap=dens_sin)
+#' densPol_ful = densityMapToPolygons(densityMap=dens_ful)
+#' polyLocations = labeledLoc
+#' polyLocations = locatePolygonPoints2(test_points=polyLocations,polygonA=densPol_sin,
+#'    crs="+proj=longlat +ellps=WGS84",setcoord = TRUE,nameA="sinuatus")
+locatePolygonPoints2 = function(test_points,
+                                polygonA,
+                                crs = "+proj=longlat +ellps=WGS84",
+                                setcoord = TRUE,
+                                nameA = "A") {
+  pts = test_points
+  pts$longitude = as.numeric(pts$longitude)
+  pts$latitude = as.numeric(pts$latitude)
+  if (setcoord == T) {
+    sp::coordinates(pts) = ~ longitude + latitude
+    sp::proj4string(pts) = sp::CRS(crs)
+    polygonA = sp::spTransform(polygonA, crs)
+  }
+  overpolygon = cbind(sp::over(pts, polygonA))
+  overpolygon[is.na(overpolygon)] = 0 
+  colnames(overpolygon) = nameA
+  test_points = cbind(test_points,overpolygon)
+  return(test_points)
+  
+}
+
 subspeciesMatchChecker = function(locfile, subsppNames) {
   #print("a")
   locWithSubspecies = locfile
@@ -1604,17 +1658,36 @@ databaseToAssignedSubspecies = function(spp,
   ## TODO: this is hanging 
   polyLocations = labeledLoc
   
-  for (slotA in 1:length(subsppNames)) {
-    for (slotB in 1:length(subsppNames)) {
-      if (subsppNames[[slotA]] != "unknown" && subsppNames[[slotB]] != "unknown" && slotA != slotB) {
-        print(paste(slotA,slotB,sep=" "))
-        polyLocations = locatePolygonPoints(test_points = polyLocations,
-                                            polygonA = densityPolygons_trim[[slotA]],polygonB = densityPolygons_trim[[slotB]],
-                                            nameA = subsppNames[[slotA]],nameB = subsppNames[[slotB]],
-                                            setcoord = T)
-      }
+  ## this is taking a long time 
+  ## we are gonna try something else
+  
+
+  
+  
+  
+ ## iterate through each polygon
+  for(polygonSlot in 1:length(subsppNames)){
+    if (subsppNames[[polygonSlot]] != "unknown"){
+      polygonA=densityPolygons_trim[[polygonSlot]]
+      polyLocations = locatePolygonPoints2(test_points = polyLocations,
+                                           polygonA = densityPolygons_trim[[polygonSlot]],
+                                           nameA = subsppNames[[polygonSlot]],
+                                           setcoord = T)
+      
     }
   }
+  # 
+  # for (slotA in 1:length(subsppNames)) {
+  #   for (slotB in 1:length(subsppNames)) {
+  #     if (subsppNames[[slotA]] != "unknown" && subsppNames[[slotB]] != "unknown" && slotA != slotB) {
+  #       print(paste(slotA,slotB,sep=" "))
+  #       polyLocations = locatePolygonPoints(test_points = polyLocations,
+  #                                           polygonA = densityPolygons_trim[[slotA]],polygonB = densityPolygons_trim[[slotB]],
+  #                                           nameA = subsppNames[[slotA]],nameB = subsppNames[[slotB]],
+  #                                           setcoord = T)
+  #     }
+  #   }
+  # }
   
   
   
