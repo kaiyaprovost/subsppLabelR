28652c9f0331b3065df7bdc9da8724eaea723b5c
kaiyaprovost <17089935+kaiyaprovost@users.noreply.github.com>
2023-09-22 13:47:49 -0400

    fix roxygen

--- final_data_Rscript.R
+++ final_data_Rscript.R
@@ -10,27 +10,29 @@ dbToQuery=c("gbif","inat","bison","vertnet")
 #EBIRD_KEY = "f49839r87f7g"
 if(redo==T) {
   species_list = c("Phainopepla nitens",
-                   #"Campylorhynchus brunneicapillus","Amphispiza bilineata",
-                   #"Vireo bellii","Toxostoma crissale","Toxostoma curvirostre","Polioptila melanura",
-                   "Cardinalis sinuatus"#,
-                   #"Melospiza melodia","Zonotrichia leucophrys","Geococcyx californianus","Geococcyx",
-                   #"Melozone fusca","Auriparus flaviceps"
+                   "Campylorhynchus brunneicapillus","Amphispiza bilineata",
+                   "Vireo bellii","Toxostoma crissale","Toxostoma curvirostre","Polioptila melanura",
+                   "Cardinalis sinuatus",
+                   "Melospiza melodia","Zonotrichia leucophrys","Geococcyx californianus","Geococcyx",
+                   "Melozone fusca","Auriparus flaviceps",
+                   NULL
   )
 
   subspp_list = list(c("lepida","nitens"),
-                     #c("affinis","brunneicapillus","anthonyi","bryanti","guttatus","sandiegensis","seri"),
-                     #c("bangsi","bilineata","belvederei","cana","deserticola","carmenae","grisea","opuntia","pacifica","tortugae"),
-                     #c("medius","bellii","arizonae","pusillus"),
-                     #c("coloradense","crissale","dumosum","trinitatis"),
-                     #c("celsum","curvirostre","insularum","maculatum","oberholseri","occidentale","palmeri"),
-                     #c("curtata","melanura","lucida"),
-                     c("sinuatus","fulvescens","peninsulae")#,
-                     #c("adusta","amaka","atlantica","beata","caurina","clementae","cleonensis","cooperi","coronatorum","euphonia","fallax","fisherella","goldmani","gouldii","graminea","heermanni","inexspectata","insignis","juddi","kenaiensis","mailliardi","maxillaris","maxima","melodia","merrilli","mexicana","micronyx","montana","morphna","pectoralis","pusillula","rivularis","rufina","saltonis","samuelis","samuelsis","sanaka","santaecrucis","villai","yuriria"),
-                     #c("leucophrys","gambelii","nuttalli","pugetensis","oriantha"),
-                     #NULL,
-                     #c("californianus","velox"),
-                     #c("campoi","fusca","intermedia","jamesi","mesata","mesoleuca","perpallida","potosina","texana","toroi"),
-                     #c("acaciarum","flaviceps","hidalgensis","lamprocephalus","ornatus","sinaloae")
+                     c("affinis","brunneicapillus","anthonyi","bryanti","guttatus","sandiegensis","seri"),
+                     c("bangsi","bilineata","belvederei","cana","deserticola","carmenae","grisea","opuntia","pacifica","tortugae"),
+                     c("medius","bellii","arizonae","pusillus"),
+                     c("coloradense","crissale","dumosum","trinitatis"),
+                     c("celsum","curvirostre","insularum","maculatum","oberholseri","occidentale","palmeri"),
+                     c("curtata","melanura","lucida"),
+                     c("sinuatus","fulvescens","peninsulae"),
+                     c("adusta","amaka","atlantica","beata","caurina","clementae","cleonensis","cooperi","coronatorum","euphonia","fallax","fisherella","goldmani","gouldii","graminea","heermanni","inexspectata","insignis","juddi","kenaiensis","mailliardi","maxillaris","maxima","melodia","merrilli","mexicana","micronyx","montana","morphna","pectoralis","pusillula","rivularis","rufina","saltonis","samuelis","samuelsis","sanaka","santaecrucis","villai","yuriria"),
+                     c("leucophrys","gambelii","nuttalli","pugetensis","oriantha"),
+                     NULL,
+                     c("californianus","velox"),
+                     c("campoi","fusca","intermedia","jamesi","mesata","mesoleuca","perpallida","potosina","texana","toroi"),
+                     c("acaciarum","flaviceps","hidalgensis","lamprocephalus","ornatus","sinaloae"),
+                     NULL
   )
 
   for(i in 1:length(species_list)) {
@@ -41,43 +43,43 @@ if(redo==T) {
     print(subspp)
 
     dir.create(paste("C:/Users/kaiya/Documents/GitHub/subsppLabelR/",spp,"_",quant,"/",sep=""))
-    if(!(file.exists(paste("C:/Users/kaiya/Documents/GitHub/subsppLabelR/",spp,"_",quant,"/",spp,"_",quant,"_subspplabelR_RAW.txt",sep="")))){
-      listFromSubspeciesOcc = subspeciesOccQuery(spp=spp,subsppList=subspp,pointLimit=pointLimit,dbToQuery=dbToQuery)
-      labeledLoc = labelSubspecies(subsppOccList=listFromSubspeciesOcc)
-      head(labeledLoc)
-      write.table(labeledLoc,paste("C:/Users/kaiya/Documents/GitHub/subsppLabelR/",spp,"_",quant,"/",spp,"_",quant,"_subspplabelR_RAW.txt",sep=""),sep="\t",row.names = F)
-    } else {
-      labeledLoc = read.table(paste("C:/Users/kaiya/Documents/GitHub/subsppLabelR/",spp,"_",quant,"/",spp,"_",quant,"_subspplabelR_RAW.txt",sep=""),sep="\t",header=T)
-    }
+    # if(!(file.exists(paste("C:/Users/kaiya/Documents/GitHub/subsppLabelR/",spp,"_",quant,"/",spp,"_",quant,"_subspplabelR_RAW.txt",sep="")))){
+    #   listFromSubspeciesOcc = subspeciesOccQuery(spp=spp,subsppList=subspp,pointLimit=pointLimit,dbToQuery=dbToQuery)
+    #   labeledLoc = labelSubspecies(subsppOccList=listFromSubspeciesOcc)
+    #   head(labeledLoc)
+    #   write.table(labeledLoc,paste("C:/Users/kaiya/Documents/GitHub/subsppLabelR/",spp,"_",quant,"/",spp,"_",quant,"_subspplabelR_RAW.txt",sep=""),sep="\t",row.names = F)
+    # } else {
+    #   labeledLoc = read.table(paste("C:/Users/kaiya/Documents/GitHub/subsppLabelR/",spp,"_",quant,"/",spp,"_",quant,"_subspplabelR_RAW.txt",sep=""),sep="\t",header=T)
+    # }
 
     print(i)
   }
-  for (i in 1:length(species_list)){
-    print(i)
-    spp = species_list[i]
-    subspp = subspp_list[[i]]
-    print(spp)
-    print(subspp)
-    if(!(file.exists(paste("C:/Users/kaiya/Documents/GitHub/subsppLabelR/",spp,"_",quant,"/",spp,"_",quant,"_subspplabelR_loc_good.txt",sep="")))){
-      labeledLoc$longitude = as.numeric(labeledLoc$longitude)
-      labeledLoc$latitude = as.numeric(labeledLoc$latitude)
-      labeledLoc$subspecies = as.factor(labeledLoc$subspecies)
-
-      final = subsppLabelR::databaseToAssignedSubspecies(spp=spp,
-                                                         subsppList = subspp,
-                                                         pointLimit=pointLimit,dbToQuery=dbToQuery,
-                                                         quantile=quant,
-                                                         plotIt=T,
-                                                         datafile = labeledLoc,
-                                                         outputDir="C:/Users/kaiya/Documents/GitHub/subsppLabelR/")
-      final$loc_suspect
-      final$loc_good
-      final$pol
-      write.table(final$loc_suspect,paste("C:/Users/kaiya/Documents/GitHub/subsppLabelR/",spp,"_",quant,"/",spp,"_",quant,"_subspplabelR_loc_suspect.txt",sep=""),sep="\t",row.names = F)
-      write.table(final$loc_good,paste("C:/Users/kaiya/Documents/GitHub/subsppLabelR/",spp,"_",quant,"/",spp,"_",quant,"_subspplabelR_loc_good.txt",sep=""),sep="\t",row.names = F)
-    }
-    print(i)
-  }
+  # for (i in 1:length(species_list)){
+  #   print(i)
+  #   spp = species_list[i]
+  #   subspp = subspp_list[[i]]
+  #   print(spp)
+  #   print(subspp)
+  #   if(!(file.exists(paste("C:/Users/kaiya/Documents/GitHub/subsppLabelR/",spp,"_",quant,"/",spp,"_",quant,"_subspplabelR_loc_good.txt",sep="")))){
+  #     labeledLoc$longitude = as.numeric(labeledLoc$longitude)
+  #     labeledLoc$latitude = as.numeric(labeledLoc$latitude)
+  #     labeledLoc$subspecies = as.factor(labeledLoc$subspecies)
+  #
+  #     final = subsppLabelR::databaseToAssignedSubspecies(spp=spp,
+  #                                                        subsppList = subspp,
+  #                                                        pointLimit=pointLimit,dbToQuery=dbToQuery,
+  #                                                        quantile=quant,
+  #                                                        plotIt=T,
+  #                                                        datafile = labeledLoc,
+  #                                                        outputDir="C:/Users/kaiya/Documents/GitHub/subsppLabelR/")
+  #     final$loc_suspect
+  #     final$loc_good
+  #     final$pol
+  #     write.table(final$loc_suspect,paste("C:/Users/kaiya/Documents/GitHub/subsppLabelR/",spp,"_",quant,"/",spp,"_",quant,"_subspplabelR_loc_suspect.txt",sep=""),sep="\t",row.names = F)
+  #     write.table(final$loc_good,paste("C:/Users/kaiya/Documents/GitHub/subsppLabelR/",spp,"_",quant,"/",spp,"_",quant,"_subspplabelR_loc_good.txt",sep=""),sep="\t",row.names = F)
+  #   }
+  #   print(i)
+  # }
 }
 ## now get the files
 occfiles=list.files(path="C:/Users/kaiya/Documents/GitHub/subsppLabelR/",
