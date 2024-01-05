f4932faaddbeeb4710ab411b84899823591aa750
kaiyaprovost <17089935+kaiyaprovost@users.noreply.github.com>
2024-01-05 12:08:38 -0500

    testing code for checkpointing

--- R/CalculateNicheSubspeciesDifferences.R
+++ R/CalculateNicheSubspeciesDifferences.R
@@ -446,6 +446,7 @@ localitiesToNicheMath = function(Env,loc,species,rep1=10,rep2=1000,
   bg_bg_filename = paste(species,"_bg_bg.txt",sep="")
 
   if(!file.exists(bg_dat_filename) | !file.exists(bg_bg_filename) | overwrite==T) {
+    print("Making bg_dat and bg_bg")
     loc_thin_bgstuff = backgroundForPCA(localities = loc_thin,e=Env)
     bg_dat = loc_thin_bgstuff$bgenv
     bg_bg = loc_thin_bgstuff$bgpoints
@@ -457,126 +458,146 @@ localitiesToNicheMath = function(Env,loc,species,rep1=10,rep2=1000,
     bg_bg = read.table(bg_bg_filename)
   }
 
-  ## check if the files exist
-  do_files_exist==T
-  for(name in all_subspp){
-    print(name)
-    file1 = paste(species,"_",name,"_bgext_by_subspecies.txt",sep="")
-    file2 = paste(species,"_",name,"_bgenv_by_subspecies.txt",sep="")
-    file3 = paste(species,"_",name,"_bgpoints_by_subspecies.txt",sep="")
-    file4 = paste(species,"_",name,"_bgextbg_by_subspecies.txt",sep="")
-    if(!(file.exists(file1))) {
-      do_files_exist = F
-      print(paste("MISSING",file1))
-    }
-    if(!(file.exists(file2))) {
-      do_files_exist = F
-      print(paste("MISSING",file2))
-    }
-    if(!(file.exists(file3))) {
-      do_files_exist = F
-      print(paste("MISSING",file3))
-    }
-    if(!(file.exists(file4))) {
-      do_files_exist = F
-      print(paste("MISSING",file4))
-    }
+  ## NOTE: I do not know why these two processes are not identical. But they are not, so I am going to remove this code.
 
-  }
+  # ## check if the files exist
+  # do_files_exist=T
+  # for(name in all_subspp){
+  #   print(name)
+  #   file1 = paste(species,"_",name,"_bgext_by_subspecies.txt",sep="")
+  #   file2 = paste(species,"_",name,"_bgenv_by_subspecies.txt",sep="")
+  #   file3 = paste(species,"_",name,"_bgpoints_by_subspecies.txt",sep="")
+  #   file4 = paste(species,"_",name,"_bgextbg_by_subspecies.txt",sep="")
+  #   if(!(file.exists(file1))) {
+  #     do_files_exist = F
+  #     print(paste("MISSING",file1))
+  #   }
+  #   if(!(file.exists(file2))) {
+  #     do_files_exist = F
+  #     print(paste("MISSING",file2))
+  #   }
+  #   if(!(file.exists(file3))) {
+  #     do_files_exist = F
+  #     print(paste("MISSING",file3))
+  #   }
+  #   if(!(file.exists(file4))) {
+  #     do_files_exist = F
+  #     print(paste("MISSING",file4))
+  #   }
+  #
+  # }
+  #
+  # if(do_files_exist==F) {
+  #   print("NOT ALL FILES EXIST")
 
-  if(do_files_exist==F) {
-    print("NOT ALL FILES EXIST")
-    perspecies_bgstuff = backgroundPerSpecies(localities = loc_thin,e=Env)
+  perspecies_bgstuff = backgroundPerSpecies(localities = loc_thin,e=Env)
 
-    bgenv_by_subspecies = perspecies_bgstuff$bgenv_by_subspecies
-    ## bgenv_by_subspecies has list of one df per subspecies
-    lapply(1:length(bgenv_by_subspecies), FUN=function(i) {
-      ## iterates over the df
-      name = names(bgenv_by_subspecies)[i]
-      df = bgenv_by_subspecies[[i]]
-      bgenv_sub_filename = paste(species,"_",name,"_bgenv_by_subspecies.txt",sep="")
-      write.table(df,bgenv_sub_filename)
-    })
+  bgenv_by_subspecies = perspecies_bgstuff$bgenv_by_subspecies
+  ## bgenv_by_subspecies has list of one df per subspecies
+  lapply(1:length(bgenv_by_subspecies), FUN=function(i) {
+    ## iterates over the df
+    name = names(bgenv_by_subspecies)[i]
+    df = bgenv_by_subspecies[[i]]
+    bgenv_sub_filename = paste(species,"_",name,"_bgenv_by_subspecies.txt",sep="")
+    write.table(df,bgenv_sub_filename,row.names = F)
+  })
 
-    bgext_by_subspecies = perspecies_bgstuff$bgext_by_subspecies
-    ## bgext_by_subspecies has list of one df per subspecies
-    lapply(1:length(bgext_by_subspecies), FUN=function(i) {
-      ## iterates over the df
-      name = names(bgext_by_subspecies)[i]
-      df = bgext_by_subspecies[[i]]
-      bgext_sub_filename = paste(species,"_",name,"_bgext_by_subspecies.txt",sep="")
-      write.table(df,bgext_sub_filename)
-    })
+  bgext_by_subspecies = perspecies_bgstuff$bgext_by_subspecies
+  ## bgext_by_subspecies has list of one df per subspecies
+  lapply(1:length(bgext_by_subspecies), FUN=function(i) {
+    ## iterates over the df
+    name = names(bgext_by_subspecies)[i]
+    df = bgext_by_subspecies[[i]]
+    bgext_sub_filename = paste(species,"_",name,"_bgext_by_subspecies.txt",sep="")
+    write.table(df,bgext_sub_filename,row.names = F)
+  })
 
-    bgpoints_by_subspecies = perspecies_bgstuff$bgpoints_by_subspecies
-    ## bgpoints_by_subspecies has list of one df per subspecies
-    lapply(1:length(bgpoints_by_subspecies), FUN=function(i) {
-      ## iterates over the df
-      name = names(bgpoints_by_subspecies)[i]
-      df = bgpoints_by_subspecies[[i]]
-      bgpoints_sub_filename = paste(species,"_",name,"_bgpoints_by_subspecies.txt",sep="")
-      write.table(df,bgpoints_sub_filename)
-    })
+  bgpoints_by_subspecies = perspecies_bgstuff$bgpoints_by_subspecies
+  ## bgpoints_by_subspecies has list of one df per subspecies
+  lapply(1:length(bgpoints_by_subspecies), FUN=function(i) {
+    ## iterates over the df
+    name = names(bgpoints_by_subspecies)[i]
+    df = bgpoints_by_subspecies[[i]]
+    bgpoints_sub_filename = paste(species,"_",name,"_bgpoints_by_subspecies.txt",sep="")
+    write.table(df,bgpoints_sub_filename,row.names = F)
+  })
 
-    bgextbg_by_subspecies = perspecies_bgstuff$bgextbg_by_subspecies
-    ## bgextbg_by_subspecies has list of one df per subspecies
-    lapply(1:length(bgextbg_by_subspecies), FUN=function(i) {
-      ## iterates over the df
-      name = names(bgextbg_by_subspecies)[i]
-      df = bgextbg_by_subspecies[[i]]
-      bgextbg_sub_filename = paste(species,"_",name,"_bgextbg_by_subspecies.txt",sep="")
-      write.table(df,bgextbg_sub_filename)
-    })
+  bgextbg_by_subspecies = perspecies_bgstuff$bgextbg_by_subspecies
+  ## bgextbg_by_subspecies has list of one df per subspecies
+  lapply(1:length(bgextbg_by_subspecies), FUN=function(i) {
+    ## iterates over the df
+    name = names(bgextbg_by_subspecies)[i]
+    df = bgextbg_by_subspecies[[i]]
+    bgextbg_sub_filename = paste(species,"_",name,"_bgextbg_by_subspecies.txt",sep="")
+    write.table(df,bgextbg_sub_filename,row.names = F)
+  })
 
-  } else {
-    print("ALL backgroundPerSpecies FILES EXIST, IMPORTING")
-    ## make a list of the bgenv
-    ## need to get the names from the files
+  # } else {
+  #   print("ALL backgroundPerSpecies FILES EXIST, IMPORTING")
+  #   ## make a list of the bgenv
+  #   ## need to get the names from the files
+  #   bgext_files = list.files(pattern=paste(species,"_.+_bgext_by_subspecies.txt",sep=""))
+  #   bgext_files_names = gsub(species,"",bgext_files)
+  #   bgext_files_names = gsub("_bgext_by_subspecies.txt","",bgext_files_names)
+  #   bgext_files_names = gsub("_","",bgext_files_names)
+  #
+  #   bgenv_files = list.files(pattern=paste(species,"_.+_bgenv_by_subspecies.txt",sep=""))
+  #   bgenv_files_names = gsub(species,"",bgenv_files)
+  #   bgenv_files_names = gsub("_bgenv_by_subspecies.txt","",bgenv_files_names)
+  #   bgenv_files_names = gsub("_","",bgenv_files_names)
+  #
+  #   bgpoints_files = list.files(pattern=paste(species,"_.+_bgpoints_by_subspecies.txt",sep=""))
+  #   bgpoints_files_names = gsub(species,"",bgpoints_files)
+  #   bgpoints_files_names = gsub("_bgpoints_by_subspecies.txt","",bgpoints_files_names)
+  #   bgpoints_files_names = gsub("_","",bgpoints_files_names)
+  #
+  #   bgextbg_files = list.files(pattern=paste(species,"_.+_bgextbg_by_subspecies.txt",sep=""))
+  #   bgextbg_files_names = gsub(species,"",bgextbg_files)
+  #   bgextbg_files_names = gsub("_bgextbg_by_subspecies.txt","",bgextbg_files_names)
+  #   bgextbg_files_names = gsub("_","",bgextbg_files_names)
+  #
+  #   ## now read them all in
+  #   bgenv_by_subspecies = lapply(bgenv_files,FUN=function(i){
+  #     #df = read.table(i,header=T,colClasses=c(rep("numeric",13),rep("integer",3),"numeric",rep("integer",3,5)))
+  #     df = read.table(i,header=T,colClasses="numeric")
+  #
+  #     ## need to specify that some of them are ints not num
+  #
+  #   })
+  #   names(bgenv_by_subspecies) = bgenv_files_names
+  #
+  #   bgext_by_subspecies = lapply(bgext_files,FUN=function(i){
+  #     #df = read.table(i,header=T,colClasses=c(rep("numeric",13),rep("integer",3),"numeric",rep("integer",3,5)))
+  #     df = read.table(i,header=T,colClasses="numeric")
+  #
+  #     ## need to specify that some of them are ints not num
+  #   })
+  #   names(bgext_by_subspecies) = bgext_files_names
+  #
+  #   bgpoints_by_subspecies = lapply(bgpoints_files,FUN=function(i){
+  #     df = read.table(i,header=T)
+  #     })
+  #   names(bgpoints_by_subspecies) = bgpoints_files_names
+  #
+  #   bgextbg_by_subspecies = lapply(bgextbg_files,FUN=function(i){
+  #     #df = read.table(i,header=T,colClasses=c(rep("numeric",13),rep("integer",3),"numeric",rep("integer",3,5)))
+  #     df = read.table(i,header=T,colClasses="numeric")
+  #
+  #     ## need to specify that some of them are ints not num
+  #   })
+  #   names(bgextbg_by_subspecies) = bgextbg_files_names
+  #
+  #   ## make a list of the bge stuff
+  #
+  #   ## combine them together into the object
+  #   perspecies_bgstuff = list(bgenv_by_subspecies=bgenv_by_subspecies,
+  #                             bgext_by_subspecies=bgext_by_subspecies,
+  #                             bgpoints_by_subspecies=bgpoints_by_subspecies,
+  #                             bgextbg_by_subspecies=bgextbg_by_subspecies)
+  #
+  # }
 
-    bgext_files = list.files(pattern=paste(species,"_.+_bgext_by_subspecies.txt",sep=""))
-    bgext_files_names = gsub(species,"",bgext_files)
-    bgext_files_names = gsub("_bgext_by_subspecies.txt","",bgext_files_names)
-    bgext_files_names = gsub("_","",bgext_files_names)
-
-    bgenv_files = list.files(pattern=paste(species,"_.+_bgenv_by_subspecies.txt",sep=""))
-    bgenv_files_names = gsub(species,"",bgenv_files)
-    bgenv_files_names = gsub("_bgenv_by_subspecies.txt","",bgenv_files_names)
-    bgenv_files_names = gsub("_","",bgenv_files_names)
-
-    bgpoints_files = list.files(pattern=paste(species,"_.+_bgpoints_by_subspecies.txt",sep=""))
-    bgpoints_files_names = gsub(species,"",bgpoints_files)
-    bgpoints_files_names = gsub("_bgpoints_by_subspecies.txt","",bgpoints_files_names)
-    bgpoints_files_names = gsub("_","",bgpoints_files_names)
-
-    bgextbg_files = list.files(pattern=paste(species,"_.+_bgextbg_by_subspecies.txt",sep=""))
-    bgextbg_files_names = gsub(species,"",bgextbg_files)
-    bgextbg_files_names = gsub("_bgextbg_by_subspecies.txt","",bgextbg_files_names)
-    bgextbg_files_names = gsub("_","",bgextbg_files_names)
-
-    ## now read them all in
-    bgenv_by_subspecies = lapply(bgenv_files,FUN=function(i){ df = read.table(i) })
-    names(bgenv_by_subspecies) = bgenv_files_names
-
-    bgext_by_subspecies = lapply(bgext_files,FUN=function(i){ df = read.table(i) })
-    names(bgext_by_subspecies) = bgext_files_names
-
-    bgpoints_by_subspecies = lapply(bgpoints_files,FUN=function(i){ df = read.table(i) })
-    names(bgpoints_by_subspecies) = bgpoints_files_names
-
-    bgextbg_by_subspecies = lapply(bgextbg_files,FUN=function(i){ df = read.table(i) })
-    names(bgextbg_by_subspecies) = bgextbg_files_names
-
-    ## make a list of the bge stuff
-
-    ## combine them together into the object
-    perspecies_bgstuff = list(bgenv_by_subspecies=bgenv_by_subspecies,
-                              bgext_by_subspecies=bgext_by_subspecies,
-                              bgpoints_by_subspecies=bgpoints_by_subspecies,
-                              bgextbg_by_subspecies=bgextbg_by_subspecies)
-
-  }
-
-  pcaOutput = createPcaToCompare(loc_thin_bgstuff,perspecies_bgstuff,species)
+  pcaOutput = createPcaToCompare(loc_thin_bgstuff,perspecies_bgstuff,species) ## apparently not fixed yet
   pca_grid_clim = pcaOutput$grid_clim
   if(verbose==T){print("finished createPcaToCompare")}
   overlap_filename = paste(species,"_overlap.txt",sep="")
