845ca9a0efc04d69367980863a9355873bcc51b7
kaiyaprovost <17089935+kaiyaprovost@users.noreply.github.com>
2024-07-18 14:55:24 -0400

    Update final_data_Rscript.R

--- scripts/final_data_Rscript.R
+++ scripts/final_data_Rscript.R
@@ -3,14 +3,19 @@
 ## TODO: unit testing
 
 detach("package:subsppLabelR", unload = TRUE)
-#devtools::install_github('kaiyaprovost/subsppLabelR',force=T)
-install.packages("C:/Users/kaiya/Documents/GitHub/subsppLabelR/",repos=NULL,type="source",force=T)
+devtools::install_github('kaiyaprovost/subsppLabelR',force=T)
+#install.packages(folder,repos=NULL,type="source",force=T)
 library(subsppLabelR)
+
+#folder = "C:/Users/kaiya/Documents/GitHub/subsppLabelR"
+folder = "/Users/kprovost/Documents/GitHub/subsppLabelR/"
+setwd(folder)
+
 ## parameters
-redo=F; overwrite=F
-pointLimit=10000
+redo=T; overwrite=F
+pointLimit=2000
 #quant_list=sort(c(seq(0.05,0.95,0.05),0.99,0.33,0.67))
-quant_list=c(0.80)
+quant_list=c(0.5)
 dbToQuery=c("gbif","inat","bison","vertnet") #EBIRD_KEY = "f49839r87f7g"
 xmin = -180; xmax = -50; ymin = 0; ymax = 75;
 if(redo==T) {
@@ -18,15 +23,23 @@ if(redo==T) {
     #"Cardinalis cardinalis",
     #"Phainopepla nitens",
     #"Cardinalis sinuatus",
-    #"Campylorhynchus brunneicapillus","Amphispiza bilineata",
-    #"Vireo bellii","Toxostoma crissale","Toxostoma curvirostre","Polioptila melanura",
-    #"Melospiza melodia","Zonotrichia leucophrys","Geococcyx californianus",
-    "Geococcyx",
-    #"Melozone fusca","Auriparus flaviceps",
+    #"Campylorhynchus brunneicapillus",
+    #"Amphispiza bilineata",
+    #"Vireo bellii",
+    #"Toxostoma crissale",
+    #"Toxostoma curvirostre",
+    #"Polioptila melanura",
+    #"Melospiza melodia",
+    #"Zonotrichia leucophrys",
+    "Geococcyx californianus",
+    #"Geococcyx",
+    #"Melozone fusca",
+    #"Auriparus flaviceps",
     NULL
   )
 
-  subspp_list = list(#c("affinis","canicaudus","cardinalis","carneus","clintoni", "coccineus","flammiger","floridanus","igneus","littoralis", "magnirostris","mariae", "phillipsi","saturatus","seftoni","sinaloensis","superbus","townsendi","yucatanicus"),
+  subspp_list = list(
+    #c("affinis","canicaudus","cardinalis","carneus","clintoni", "coccineus","flammiger","floridanus","igneus","littoralis", "magnirostris","mariae", "phillipsi","saturatus","seftoni","sinaloensis","superbus","townsendi","yucatanicus"),
     #c("lepida","nitens"),
     #c("fulvescens","peninsulae","sinuatus"),
     #c("affinis","brunneicapillus","anthonyi","bryanti","guttatus","sandiegensis","seri"),
@@ -37,8 +50,8 @@ if(redo==T) {
     #c("curtata","melanura","lucida"),
     #c("adusta","amaka","atlantica","beata","caurina","clementae","cleonensis","cooperi","coronatorum","euphonia","fallax","fisherella","goldmani","gouldii","graminea","heermanni","inexspectata","insignis","juddi","kenaiensis","mailliardi","maxillaris","maxima","melodia","merrilli","mexicana","micronyx","montana","morphna","pectoralis","pusillula","rivularis","rufina","saltonis","samuelis","samuelsis","sanaka","santaecrucis","villai","yuriria"),
     # c("leucophrys","gambelii","nuttalli","pugetensis","oriantha"),
-    #NULL,
-    c("californianus","velox"),
+    NULL,
+    #c("californianus","velox"),
     #c("campoi","fusca","intermedia","jamesi","mesata","mesoleuca","perpallida","potosina","texana","toroi"),
     #c("acaciarum","flaviceps","hidalgensis","lamprocephalus","ornatus","sinaloae"),
     NULL
@@ -50,22 +63,22 @@ if(redo==T) {
     subspp = subspp_list[[i]]
     print(spp)
     print(subspp)
-    dir.create(paste("C:/Users/kaiya/Documents/GitHub/subsppLabelR/",spp,"/",sep=""))
-    if(!(file.exists(paste("C:/Users/kaiya/Documents/GitHub/subsppLabelR/",spp,"/",spp,"_subspplabelR_RAW.txt",sep="")))){
+    dir.create(paste(folder,spp,"/",sep=""))
+    if(!(file.exists(paste(folder,spp,"/",spp,"_subspplabelR_RAW.txt",sep="")))){
       listFromSubspeciesOcc = subspeciesOccQuery(spp=spp,subsppList=subspp,pointLimit=pointLimit,dbToQuery=dbToQuery)
       labeledLoc = labelSubspecies(subsppOccList=listFromSubspeciesOcc,
                                    spp=spp,subsppList=subspp)
       head(labeledLoc)
-      write.table(labeledLoc,paste("C:/Users/kaiya/Documents/GitHub/subsppLabelR/",spp,"/",spp,"_subspplabelR_RAW.txt",sep=""),sep="\t",row.names = F)
+      write.table(labeledLoc,paste(folder,spp,"/",spp,"_subspplabelR_RAW.txt",sep=""),sep="\t",row.names = F)
     } else {
-      labeledLoc = read.table(paste("C:/Users/kaiya/Documents/GitHub/subsppLabelR/",spp,"/",spp,"_subspplabelR_RAW.txt",sep=""),sep="\t",header=T)
+      labeledLoc = read.table(paste(folder,spp,"/",spp,"_subspplabelR_RAW.txt",sep=""),sep="\t",header=T)
     }
     subspp_new = unique(labeledLoc$subspecies)
     subspp_new = subspp_new[subspp_new!="unknown"]
 
     for(quant in quant_list){
       print(quant)
-      if(overwrite==T | !(file.exists(paste("C:/Users/kaiya/Documents/GitHub/subsppLabelR/",spp,"/",spp,"_",quant,"_subspplabelR_loc_good.txt",sep="")))){
+      if(overwrite==T | !(file.exists(paste(folder,spp,"/",spp,"_",quant,"_subspplabelR_loc_good.txt",sep="")))){
         labeledLoc$longitude = as.numeric(labeledLoc$longitude)
         labeledLoc$latitude = as.numeric(labeledLoc$latitude)
         labeledLoc$subspecies = as.factor(labeledLoc$subspecies)
@@ -76,24 +89,24 @@ if(redo==T) {
                                                            plotIt=T,
                                                            datafile = labeledLoc,
                                                            xmin=xmin,xmax=xmax,ymin=ymin,ymax=ymax,
-                                                           outputDir=paste("C:/Users/kaiya/Documents/GitHub/subsppLabelR/",spp,"/",sep=""))
+                                                           outputDir=paste(folder,spp,"/",sep=""))
         final$loc_suspect
         final$loc_good
         final$pol
-        write.table(final$loc_suspect,paste("C:/Users/kaiya/Documents/GitHub/subsppLabelR/",spp,"/",spp,"_",quant,"_subspplabelR_loc_suspect.txt",sep=""),sep="\t",row.names = F)
-        write.table(final$loc_good,paste("C:/Users/kaiya/Documents/GitHub/subsppLabelR/",spp,"/",spp,"_",quant,"_subspplabelR_loc_good.txt",sep=""),sep="\t",row.names = F)
+        write.table(final$loc_suspect,paste(folder,spp,"/",spp,"_",quant,"_subspplabelR_loc_suspect.txt",sep=""),sep="\t",row.names = F)
+        write.table(final$loc_good,paste(folder,spp,"/",spp,"_",quant,"_subspplabelR_loc_good.txt",sep=""),sep="\t",row.names = F)
         loc_all = rbind(final$loc_suspect,final$loc_good)
-        write.table(loc_all,paste("C:/Users/kaiya/Documents/GitHub/subsppLabelR/",spp,"/",spp,"_",quant,"_subspplabelR_loc_all.txt",sep=""),sep="\t",row.names = F)
+        write.table(loc_all,paste(folder,spp,"/",spp,"_",quant,"_subspplabelR_loc_all.txt",sep=""),sep="\t",row.names = F)
       }
     }
     print(i)
   }
 }
 ## now get the files
-setwd("C:/Users/kaiya/Documents/GitHub/subsppLabelR/")
-occfiles=list.files(path="C:/Users/kaiya/Documents/GitHub/subsppLabelR/",
+setwd(folder)
+occfiles=list.files(path=folder,
                     pattern="_subspplabelR_loc_good.txt$",recursive=T,full.names = T)
-occ_suspect_files = list.files(path="C:/Users/kaiya/Documents/GitHub/subsppLabelR/",
+occ_suspect_files = list.files(path=folder,
                                pattern="_subspplabelR_loc_suspect.txt$",recursive=T,full.names = T)
 occfiles = c(occ_suspect_files,occfiles)
 occfiles = sort(unique(occfiles))
@@ -136,7 +149,7 @@ for(i in (1:length(occfiles))){
   })
 }
 
-occ_suspect_files = list.files(path="C:/Users/kaiya/Documents/GitHub/subsppLabelR/",
+occ_suspect_files = list.files(path=folder,
                                pattern="_subspplabelR_loc_suspect.txt$",recursive=T,full.names = T)
 x = file.info(occ_suspect_files)
 occ_suspect_files = occ_suspect_files[order(x$size)]
@@ -185,8 +198,8 @@ for(j in (1:length(occ_suspect_files))){
 
 
 ## combine loc_good and loc_suspect into loc_all for each if not already exist
-occ_suspect_files = list.files(path="C:/Users/kaiya/Documents/GitHub/subsppLabelR/",
+occ_suspect_files = list.files(path=folder,
                                pattern="_subspplabelR_loc_suspect.txt$",recursive=T,full.names = T)
-occfiles=list.files(path="C:/Users/kaiya/Documents/GitHub/subsppLabelR/",
+occfiles=list.files(path=folder,
                     pattern="_subspplabelR_loc_good.txt$",recursive=T,full.names = T)
 
