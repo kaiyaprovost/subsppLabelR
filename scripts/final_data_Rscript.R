bd0d3334443e86353d97255e068df16952027ee3
kaiyaprovost <17089935+kaiyaprovost@users.noreply.github.com>
2024-07-18 15:15:43 -0400

    Update final_data_Rscript.R

--- scripts/final_data_Rscript.R
+++ scripts/final_data_Rscript.R
@@ -13,7 +13,7 @@ devtools::install_github('kaiyaprovost/subsppLabelR',force=T)
 library(subsppLabelR)
 
 ## parameters
-redo=T; overwrite=F
+redo=T; overwrite=F; downloadOnly=T
 pointLimit=10000
 #quant_list=sort(c(seq(0.05,0.95,0.05),0.99,0.33,0.67))
 quant_list=c(0.5)
@@ -38,7 +38,7 @@ if(redo==T) {
     #"Auriparus flaviceps",
     NULL
   )
-
+  
   subspp_list = list(
     #c("affinis","canicaudus","cardinalis","carneus","clintoni", "coccineus","flammiger","floridanus","igneus","littoralis", "magnirostris","mariae", "phillipsi","saturatus","seftoni","sinaloensis","superbus","townsendi","yucatanicus"),
     #c("lepida","nitens"),
@@ -57,7 +57,7 @@ if(redo==T) {
     #c("acaciarum","flaviceps","hidalgensis","lamprocephalus","ornatus","sinaloae"),
     NULL
   )
-
+  
   for(i in 1:length(species_list)) {
     print(i)
     spp = species_list[i]
@@ -65,6 +65,7 @@ if(redo==T) {
     print(spp)
     print(subspp)
     dir.create(paste(folder,spp,"/",sep=""))
+    ## if the raw file does not exist, make it. otherwise load it 
     if(!(file.exists(paste(folder,spp,"/",spp,"_subspplabelR_RAW.txt",sep="")))){
       listFromSubspeciesOcc = subspeciesOccQuery(spp=spp,subsppList=subspp,pointLimit=pointLimit,dbToQuery=dbToQuery)
       labeledLoc = labelSubspecies(subsppOccList=listFromSubspeciesOcc,
@@ -74,33 +75,42 @@ if(redo==T) {
     } else {
       labeledLoc = read.table(paste(folder,spp,"/",spp,"_subspplabelR_RAW.txt",sep=""),sep="\t",header=T)
     }
-    subspp_new = unique(labeledLoc$subspecies)
-    subspp_new = subspp_new[subspp_new!="unknown"]
-
-    for(quant in quant_list){
-      print(quant)
-      if(overwrite==T | !(file.exists(paste(folder,spp,"/",spp,"_",quant,"_subspplabelR_loc_good.txt",sep="")))){
-        labeledLoc$longitude = as.numeric(labeledLoc$longitude)
-        labeledLoc$latitude = as.numeric(labeledLoc$latitude)
-        labeledLoc$subspecies = as.factor(labeledLoc$subspecies)
-        final = subsppLabelR::databaseToAssignedSubspecies(spp=spp,
-                                                           subsppList = subspp_new,
-                                                           pointLimit=pointLimit,dbToQuery=dbToQuery,
-                                                           quant=quant,
-                                                           plotIt=T,
-                                                           datafile = labeledLoc,
-                                                           xmin=xmin,xmax=xmax,ymin=ymin,ymax=ymax,
-                                                           outputDir=paste(folder,spp,"/",sep=""))
-        final$loc_suspect
-        final$loc_good
-        final$pol
-        write.table(final$loc_suspect,paste(folder,spp,"/",spp,"_",quant,"_subspplabelR_loc_suspect.txt",sep=""),sep="\t",row.names = F)
-        write.table(final$loc_good,paste(folder,spp,"/",spp,"_",quant,"_subspplabelR_loc_good.txt",sep=""),sep="\t",row.names = F)
-        loc_all = rbind(final$loc_suspect,final$loc_good)
-        write.table(loc_all,paste(folder,spp,"/",spp,"_",quant,"_subspplabelR_loc_all.txt",sep=""),sep="\t",row.names = F)
+    
+    if(downloadOnly==F){
+      subspp_new = unique(labeledLoc$subspecies)
+      subspp_new = subspp_new[subspp_new!="unknown"]
+      
+      for(quant in quant_list){
+        print(quant)
+        if(overwrite==T | !(file.exists(paste(folder,spp,"/",spp,"_",quant,"_subspplabelR_loc_good.txt",sep="")))){
+          labeledLoc$longitude = as.numeric(labeledLoc$longitude)
+          labeledLoc$latitude = as.numeric(labeledLoc$latitude)
+          labeledLoc$subspecies = as.factor(labeledLoc$subspecies)
+          final = subsppLabelR::databaseToAssignedSubspecies(spp=spp,
+                                                             subsppList = subspp_new,
+                                                             pointLimit=pointLimit,dbToQuery=dbToQuery,
+                                                             quant=quant,
+                                                             plotIt=T,
+                                                             datafile = labeledLoc,
+                                                             xmin=xmin,xmax=xmax,ymin=ymin,ymax=ymax,
+                                                             outputDir=paste(folder,spp,"/",sep=""),
+                                                             downloadOnly=F)
+          
+          
+          final$loc_suspect
+          final$loc_good
+          final$pol
+          write.table(final$loc_suspect,paste(folder,spp,"/",spp,"_",quant,"_subspplabelR_loc_suspect.txt",sep=""),sep="\t",row.names = F)
+          write.table(final$loc_good,paste(folder,spp,"/",spp,"_",quant,"_subspplabelR_loc_good.txt",sep=""),sep="\t",row.names = F)
+          loc_all = rbind(final$loc_suspect,final$loc_good)
+          write.table(loc_all,paste(folder,spp,"/",spp,"_",quant,"_subspplabelR_loc_all.txt",sep=""),sep="\t",row.names = F)
+          
+          
+          
+        }
+        print(i)
       }
     }
-    print(i)
   }
 }
 ## now get the files
@@ -130,7 +140,7 @@ for(i in (1:length(occfiles))){
     unique(occ$assigned)
     if(nrow(occ)>0){
       #occ = occ[sample(1:nrow(occ),100),]
-
+      
       if(length(unique(occ$assigned))>1){
         print(paste("NUMBER SUBSPECIES:",length(unique(occ$assigned))))
         try({
@@ -163,18 +173,18 @@ for(j in (1:length(occ_suspect_files))){
   setwd(sus_dir)
   try({
     sus_occ=read.table(occ_suspect_files[j],header=T)
-
+    
     ## get rid of any that are subspecies unknown and assigned none
     sus_occ = sus_occ[!(sus_occ$subspecies=="unknown" & sus_occ$assigned=="none"),]
     sus_occ$assigned = sus_occ$subspecies
-
+    
     ## need to get rid of subspecies that have fewer than 10 points so the kernel can be estimated
     sus_occ = sus_occ[,c("longitude","latitude","assigned")]
     sus_occ = unique(sus_occ)
     sus_tab = table(sus_occ$assigned)
     bad_subspp = names(sus_tab)[which(sus_tab<10)]
     sus_occ = sus_occ[!(sus_occ$assigned %in% bad_subspp),]
-
+    
     print(occ_name) ## note: occ_name might accidentally be global
     unique(sus_occ$assigned)
     if(nrow(sus_occ)>0){
@@ -194,7 +204,7 @@ for(j in (1:length(occ_suspect_files))){
       print("NO LOCALITIES!")
     }
   })
-
+  
 }
 
 
