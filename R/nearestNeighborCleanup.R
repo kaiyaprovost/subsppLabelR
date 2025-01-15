dad3d9e2efaad3c7a3514c699fd3f29de80c78a8
kaiyaprovost <17089935+kaiyaprovost@users.noreply.github.com>
2025-01-15 15:22:42 -0500

    Update nearestNeighborCleanup.R

--- R/nearestNeighborCleanup.R
+++ R/nearestNeighborCleanup.R
@@ -1,6 +1,8 @@
 #' Clean points via nearest neighbors
 #'
-#' This function XXX
+#' This function calculates unlabeled points based on their assigned
+#' labels, assuming the assigned labels are correct.It calculates them
+#' based on their location in latitude and longitude space.
 #'
 #' @param good_points XX
 #' @param bad_points XX
@@ -16,7 +18,9 @@ nearestNeighborCleanup = function(good_points,bad_points,k=21){
                    test=bad_points[,c("longitude","latitude")],
                    cl=good_points[,c("assigned")],
                    k=k,
-                   use.all=T)
+                   use.all=T,
+                   prob=T)
   bad_points$KNN = pr
+  bad_points$KNNprob = attributes(pr)$prob
   return(bad_points)
 }
