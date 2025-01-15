5923ee1b43ee959a2c233c53f31810682ae26b37
kaiyaprovost <17089935+kaiyaprovost@users.noreply.github.com>
2025-01-15 14:59:39 -0500

    Update final_data_Rscript.R

--- final_data_Rscript.R
+++ final_data_Rscript.R
@@ -4,11 +4,11 @@
 
 detach("package:subsppLabelR", unload = TRUE)
 
-#pkgDirectory = "C:/Users/kaiya/Documents/GitHub/subsppLabelR/" ## WINDOWS
-pkgDirectory = "/Users/kprovost/Documents/GitHub/subsppLabelR/" ## MAC
+pkgDirectory = "C:/Users/kaiya/Documents/GitHub/subsppLabelR/" ## WINDOWS
+#pkgDirectory = "/Users/kprovost/Documents/GitHub/subsppLabelR/" ## MAC
 
 #devtools::install_github('kaiyaprovost/subsppLabelR',force=T)
-install.packages(pkgDirectory,repos=NULL,type="source",force=T) 
+install.packages(pkgDirectory,repos=NULL,type="source",force=T)
 library(subsppLabelR)
 ## parameters
 redo=F; overwrite=T
@@ -16,7 +16,7 @@ pointLimit=10000
 quant_list=c(seq(0.05,0.95,0.05),0.99,0.33,0.67)
 dbToQuery=c("gbif","inat","bison","vertnet") #EBIRD_KEY = "f49839r87f7g"
 xmin = -180; xmax = -50; ymin = 0; ymax = 75;
-if(redo==T) {
+if(redo==T) {x
   species_list = c(#"Phainopepla nitens",
     #"Cardinalis sinuatus",
     #"Campylorhynchus brunneicapillus","Amphispiza bilineata",
