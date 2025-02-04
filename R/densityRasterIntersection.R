19f634b2e757e79aa56da6ac151a0683b49b712d
kaiyaprovost <17089935+kaiyaprovost@users.noreply.github.com>
2025-02-04 12:05:59 -0500

    Remove nested functions from densityRasterIntersection
    Moved convertRelToPresAbs and loopFindClusters out of the main function

--- R/densityRasterIntersection.R
+++ R/densityRasterIntersection.R
@@ -26,8 +26,6 @@ NULL
 #'    xmin=-125,xmax=-60,ymin=10,ymax=50)
 #' densPol_sin = densityMapToPolygons(densityMap=dens_sin)
 #'
-#'
-#'
 densityRasterIntersection = function(densA,densB,verbose=F) {
   ## this needs to take in two density rasters
   ## and then it needs to remove the overlaps of the density rasters
@@ -36,7 +34,8 @@ densityRasterIntersection = function(densA,densB,verbose=F) {
   library(sf)
   library(raster)
 
-  myFolder = "C:/Users/kaiya/Documents/Work/GitHub/subsppLabelR/"
+  #myFolder = "C:/Users/kaiya/Documents/Work/GitHub/subsppLabelR/"
+  myFolder = "~/Documents/GitHub/subsppLabelR/"
   setwd(myFolder)
 
   densityStack = raster::stack("Phainopepla nitens/DensityRaster_Phainopepla nitens_0.75.tif")
@@ -47,47 +46,15 @@ densityRasterIntersection = function(densA,densB,verbose=F) {
   densAR = raster("Phainopepla nitens/Phainopepla nitens lepida_raw_raster.tif")
   densBR = raster("Phainopepla nitens/Phainopepla nitens nitens_raw_raster.tif")
 
-  presAbs = function(densA,densB) {
-    ## convert to a pres/abs
-    densA_pres = densA
-    densB_pres = densB
-    densA_pres[!(is.na(densA_pres))] = 1
-    densA_pres[(is.na(densA_pres))] = 0
-    densB_pres[!(is.na(densB_pres))] = 1
-    densB_pres[(is.na(densB_pres))] = 0
-    dens_pres_abs_overlap = densA_pres - densB_pres
-    dens_pres_abs_overlap[densA_pres==0 & densB_pres == 0] = NA
-    ## dens_pres_abs_overlap is a raster where if the two rasters overlap, it has a
-    ## value of 0. if only one raster is present on that cell, it is a -1 or +1
-    ## where it is -1 if B is present and +1 if A is present
-    return(dens_pres_abs_overlap)
-  }
-
   dens_pres_abs_overlap = presAbs(densA,densB)
   plot(dens_pres_abs_overlap) ## if 0, overlap. if -1 or +1, only on one side.
   ## note: presAbs doesn't work with the raw rasters, they are all complete
-
-  relOverlap = function(densA,densB) {
-    densA_rel = densA
-    densA_rel[is.na(densA_rel)] = 0
-    densB_rel = densB
-    densB_rel[is.na(densB_rel)] = 0
-    dens_relative_bool = densA_rel - densB_rel
-    dens_relative_bool[densA_rel==0 & densB_rel == 0] = NA
-    return(dens_relative_bool)
-  }
 
   dens_relative_values = relOverlap(densA,densB)
   dens_relative_raw_values = relOverlap(densAR,densBR)
   par(mfrow=c(1,2))
   plot(dens_relative_values)
   plot(dens_relative_raw_values)
-
-  convertRelToPresAbs = function(dens_relative_bool){
-    dens_relative_bool[dens_relative_bool>0] = 1
-    dens_relative_bool[dens_relative_bool<0] = -1
-    return(dens_relative_bool)
-  }
 
   dens_relative_bool = convertRelToPresAbs(dens_relative_values)
   dens_relative_raw_bool = convertRelToPresAbs(dens_relative_raw_values)
@@ -112,29 +79,6 @@ densityRasterIntersection = function(densA,densB,verbose=F) {
   myCells = which(!is.na(values(ras)))
   targetCells = myCells
 
-  loopFindClusters <- function(ras,allCells=which(!is.na(values(ras))),targetCells=allCells,verbose=F) {
-    visited = c()
-    clusterList = list()
-    for(cell in targetCells) {
-      if(verbose) {print("MY CELL")}
-      if(verbose) {print(cell)}
-      if(cell %in% visited) { next }  else {
-        if(verbose) {print("TARGET CELLS")}
-        if(verbose) {print(targetCells)}
-        returned = recurseNeighbors(cell=cell,visited=visited,ras=ras,allCells=allCells,verbose=verbose)
-        cluster = c(returned$cluster)
-        visited = c(returned$visited)
-        if(verbose) {print("MY CLUSTER")}
-        if(verbose) {print(cluster)}
-        clusterList = c(clusterList,list(cluster))
-        if(verbose) {print("MY VISITED")}
-        if(verbose) {print(visited)}
-      }
-
-    }
-    return(clusterList)
-  }
-
   clusterList = loopFindClusters(ras=dens_AB_rel_overlap,allCells=myCells,targetCells=myCells,verbose=F)
 
   identified_overlap_clusters = dens_AB_rel_overlap
@@ -143,11 +87,6 @@ densityRasterIntersection = function(densA,densB,verbose=F) {
     identified_overlap_clusters[clusterCells] = i
   }
   plot(identified_overlap_clusters)
-
-
-
-
-
 
   ## find the cluster with the densest cell for each raster
   ## i can use this function to check if the highest density area is connected to the polygon tho which is nice
@@ -180,8 +119,6 @@ densityRasterIntersection = function(densA,densB,verbose=F) {
   densest_A[densestClusterListA[[1]]] = 1
   plot(densest_A)
 
-
-
   par(mfrow=c(1,3))
   plot(identified_overlap_clusters,col=c("red","blue"))
   plot(densest_A); plot(identified_overlap_clusters,add=T,col="red")
@@ -192,35 +129,68 @@ densityRasterIntersection = function(densA,densB,verbose=F) {
 
   ## use the densest cluster to assign the thing
   ## for each polygon in the assigned raster, check if that polygon overlaps A and B most dense
-  identified_overlap_clusters =
+  identified_overlap_clusters
 
   for(overlapCluster in clusterList) {
     ## find which polygon this cluster belongs to on both A and B
-
-
+    ## get valid cells for A
+    valid_A = intersect(densCellsA, overlapCluster)
+    a_cluster = loopFindClusters(ras=densest_A,allCells=densCellsA,targetCells=valid_A,verbose=F)
+    ## get valid cells for B
+    valid_B = intersect(densCellsB, overlapCluster)
+    b_cluster = loopFindClusters(ras=densest_B,allCells=densCellsB,targetCells=valid_B,verbose=F)
+    ## check if these are the densest polygons for A and B
     is_A_densest = (1 %in% densest_A[overlapCluster])
     is_B_densest = (1 %in% densest_B[overlapCluster])
+    ## calculate the density values for each cluster
+    density_values_A = densA[a_cluster[[1]]]
+    density_values_B = densB[b_cluster[[1]]]
+    
     ## A is densest and B is not densest
     if(is_A_densest && !is_B_densest){
-      ## identify the polygon for B
-      ## get valid cells for B
-      valid_B = intersect(densCellsB, overlapCluster)
-      b_cluster = loopFindClusters(ras=densest_B,allCells=densCellsB,targetCells=valid_B,verbose=F)
+      ## remove those cells from B
       valid_polygon_B[b_cluster[[1]]]=NA
 
     }
     ## A is not densest and B is densest
-    if(!is_A_densest && is_B_densest){
-      ## identify the polygon for A
-      ## get valid cells for A
-      valid_A = intersect(densCellsA, overlapCluster)
-      a_cluster = loopFindClusters(ras=densest_A,allCells=densCellsA,targetCells=valid_A,verbose=F)
+    else if(!is_A_densest && is_B_densest){
+      ## remove those cells from A
       valid_polygon_A[a_cluster[[1]]]=NA
-
     }
-
     ## both A and B are not densest
+    else if(!is_A_densest && !is_B_densest){
+      ## if tied, then break the tie with which has more density and remove the 
+      ## polygon that is less dense
+      ## A is more dense, get rid of B
+      if(sum(density_values_A,na.rm=T) > sum(density_values_B,na.rm=T)) {
+        valid_polygon_B[b_cluster[[1]]]=NA
+      }
+      ## B is more dense, get rid of A
+      else if(sum(density_values_A,na.rm=T) < sum(density_values_B,na.rm=T)) {
+        valid_polygon_A[a_cluster[[1]]]=NA
+      }
+      ## they are equally dense, DIE
+      else if(sum(density_values_A,na.rm=T) == sum(density_values_B,na.rm=T)) {
+        stop("ERROR: Density rasters should differ in their sums -- check your inputs!")
+      }
+      
+    }
     ## both A and B are densest
+    else if(is_A_densest && is_B_densest){
+      ## if tied, then break the tie with which has more density, but keep both
+      ## polygons, just remove the cells based on which is more dense 
+      ## A is more dense, remove overlap cells from B
+      if(sum(density_values_A,na.rm=T) > sum(density_values_B,na.rm=T)) {
+        valid_polygon_B[overlapCluster[[1]]]=NA
+      }   
+      ## B is more dense, remove overlap cells from A
+      else if(sum(density_values_A,na.rm=T) < sum(density_values_B,na.rm=T)) {
+        valid_polygon_A[overlapCluster[[1]]]=NA
+      }  
+      ## they are equally dense, DIE
+      else if(sum(density_values_A,na.rm=T) == sum(density_values_B,na.rm=T)) {
+        stop("ERROR: Density rasters should differ in their sums -- check your inputs!")
+      }
 
   }
 
@@ -369,4 +339,95 @@ recurseNeighbors = function(cell,visited,ras,allCells,verbose=F) {
   return(list(cluster=unique(c(cluster,cell)),
               visited=unique(c(visited,cell))))
 }
-
+#' Calculate presence/absence raster from two density rasters
+#'
+#' This function XXXXXXXX
+#'
+#' @param densA XXX
+#' @param densB xXX
+#'
+#' @export
+#' @examples
+#'
+presAbs = function(densA,densB) {
+  ## convert to a pres/abs
+  densA_pres = densA
+  densB_pres = densB
+  densA_pres[!(is.na(densA_pres))] = 1
+  densA_pres[(is.na(densA_pres))] = 0
+  densB_pres[!(is.na(densB_pres))] = 1
+  densB_pres[(is.na(densB_pres))] = 0
+  dens_pres_abs_overlap = densA_pres - densB_pres
+  dens_pres_abs_overlap[densA_pres==0 & densB_pres == 0] = NA
+  ## dens_pres_abs_overlap is a raster where if the two rasters overlap, it has a
+  ## value of 0. if only one raster is present on that cell, it is a -1 or +1
+  ## where it is -1 if B is present and +1 if A is present
+  return(dens_pres_abs_overlap)
+}
+#' Calculate relative overlap raster from two density rasters
+#'
+#' This function XXXXXXXX
+#'
+#' @param densA XXX
+#' @param densB xXX
+#'
+#' @export
+#' @examples
+#'
+relOverlap = function(densA,densB) {
+  densA_rel = densA
+  densA_rel[is.na(densA_rel)] = 0
+  densB_rel = densB
+  densB_rel[is.na(densB_rel)] = 0
+  dens_relative_bool = densA_rel - densB_rel
+  dens_relative_bool[densA_rel==0 & densB_rel == 0] = NA
+  return(dens_relative_bool)
+}
+#' Loop to find recursive clusters and returns list of clusters
+#'
+#' This function XXXXXXXX
+#'
+#' @param ras XXX
+#' @param allCells xXX
+#' @param targetCells xXX 
+#' @param verbose xXX
+#'
+#' @export
+#' @examples
+#'
+loopFindClusters <- function(ras,allCells=which(!is.na(values(ras))),targetCells=allCells,verbose=F) {
+  visited = c()
+  clusterList = list()
+  for(cell in targetCells) {
+    if(verbose) {print("MY CELL")}
+    if(verbose) {print(cell)}
+    if(cell %in% visited) { next }  else {
+      if(verbose) {print("TARGET CELLS")}
+      if(verbose) {print(targetCells)}
+      returned = recurseNeighbors(cell=cell,visited=visited,ras=ras,allCells=allCells,verbose=verbose)
+      cluster = c(returned$cluster)
+      visited = c(returned$visited)
+      if(verbose) {print("MY CLUSTER")}
+      if(verbose) {print(cluster)}
+      clusterList = c(clusterList,list(cluster))
+      if(verbose) {print("MY VISITED")}
+      if(verbose) {print(visited)}
+    }
+    
+  }
+  return(clusterList)
+}
+#' Convert relative raster to presence/absence raster
+#'
+#' This function XXXXXXXX
+#'
+#' @param ras dens_relative_bool
+#'
+#' @export
+#' @examples
+#'
+convertRelToPresAbs = function(dens_relative_bool){
+  dens_relative_bool[dens_relative_bool>0] = 1
+  dens_relative_bool[dens_relative_bool<0] = -1
+  return(dens_relative_bool)
+}
