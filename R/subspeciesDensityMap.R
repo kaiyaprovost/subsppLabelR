60dc1165b22d964d688315261ddcd156d640cf1e
kaiyaprovost <17089935+kaiyaprovost@users.noreply.github.com>
2025-02-17 10:46:38 -0500

    Update subspeciesDensityMap.R

--- R/subspeciesDensityMap.R
+++ R/subspeciesDensityMap.R
@@ -38,8 +38,8 @@ subspeciesDensityMap = function(localities,
                                 ymin = NULL,
                                 ymax = NULL,
                                 total_range,
-                                relative=T,
-                                raw_raster=T,
+                                relative = T,
+                                raw_raster = T,
                                 subspp,
                                 spp,
                                 outputDir) {
@@ -48,10 +48,18 @@ subspeciesDensityMap = function(localities,
   ## TODO: allow for subspecies-specific quants
   #library(MASS)
   #library(raster)
-  if(is.null(xmin)) { xmin = min(localities$longitude,na.rm=T) }
-  if(is.null(xmax)) { xmax = max(localities$longitude,na.rm=T) }
-  if(is.null(ymin)) { ymin = min(localities$latitude,na.rm=T) }
-  if(is.null(ymax)) { ymax = max(localities$latitude,na.rm=T) }
+  if (is.null(xmin)) {
+    xmin = min(localities$longitude, na.rm = T)
+  }
+  if (is.null(xmax)) {
+    xmax = max(localities$longitude, na.rm = T)
+  }
+  if (is.null(ymin)) {
+    ymin = min(localities$latitude, na.rm = T)
+  }
+  if (is.null(ymax)) {
+    ymax = max(localities$latitude, na.rm = T)
+  }
   range = c(xmin, xmax, ymin, ymax)
   ext = raster::extent(range)
   w1 = matrix(1, 3, 3)
@@ -61,43 +69,53 @@ subspeciesDensityMap = function(localities,
     return(NULL)
   }
   density = NULL
-  try({density = MASS::kde2d(as.numeric(localities$longitude),as.numeric(localities$latitude),
-                             lims = range,n = 50)})
-  if(is.null(density)){
+  try({
+    density = MASS::kde2d(
+      as.numeric(localities$longitude),
+      as.numeric(localities$latitude),
+      lims = range,
+      n = 50
+    )
+  })
+  if (is.null(density)) {
     print("NULL")
     return(NULL)
   } else {
     ## convert to raster
     densRas = raster::raster(density)
-    if(raw_raster==T){
-      raw_file = paste(outputDir,spp," ",subspp,"_raw_raster.tif",sep="")
-      if(!file.exists(raw_file)){
-        writeRaster(densRas,paste(outputDir,spp," ",subspp,"_raw_raster.tif",sep=""),
-                    format="GTiff",overwrite=F)
+    if (raw_raster == T) {
+      raw_file = paste(outputDir, spp, " ", subspp, "_raw_raster.tif", sep = "")
+      if (!file.exists(raw_file)) {
+        writeRaster(
+          densRas,
+          paste(outputDir, spp, " ", subspp, "_raw_raster.tif", sep = ""),
+          format = "GTiff",
+          overwrite = F
+        )
       }
-
+      
     }
-
-    if(relative==T){
+    
+    if (relative == T) {
       print("rescaling")
-      values(densRas) = scales::rescale(values(densRas),c(0,1))
-      values(densRas) = round(values(densRas),digits=10)
+      values(densRas) = scales::rescale(values(densRas), c(0, 1))
+      values(densRas) = round(values(densRas), digits = 10)
     }
     ## we want to print these out before they are clipped
-
+    
     ## take the top percentile of the points, only the densest areas
     quan = quantile(densRas[densRas], quant)
     densRas_trim = densRas
     densRas_trim[densRas_trim <= quan] = NA
-    plot(densRas_trim,colNA="black",main=subspp)    #,xlim=c(xmin,xmax),ylim=c(ymin,ymax))
+    plot(densRas_trim, colNA = "black", main = subspp)    #,xlim=c(xmin,xmax),ylim=c(ymin,ymax))
     total_crs = raster::crs(total_range)
     total_ext = raster::extent(total_range)
     total_res = raster::res(total_range)
     ## crop to the existing layers
-    densRas_trim = raster::crop(densRas_trim,total_range)
-    densRas_trim = raster::projectRaster(densRas_trim,total_range)
+    densRas_trim = raster::crop(densRas_trim, total_range)
+    densRas_trim = raster::projectRaster(densRas_trim, total_range)
     raster::values(densRas_trim)[is.na(raster::values(total_range))] = NA
-
+    
     return(densRas_trim)
   }
 }
